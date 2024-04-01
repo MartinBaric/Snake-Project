f6c8db87b4d845ccdacdc6e894e8d46b36c82650
Dmitrij Sitenko <dmitrij.sitenko@iwr.uni-heidelberg.de>
2024-04-01 18:26:12 +0200

    Add_Adoptive_Snake

--- Gui_Main.cpp
+++ Gui_Main.cpp
@@ -2,23 +2,78 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 #include <random>
+//#include <vector>
 
 using namespace sf;
 
 int SIZE_X = 800;
 int SIZE_Y = 800;
+int MAX_SNAKE_SIZE = 1000;
+
+
+void draw_snake(RectangleShape* snake, VertexArray Snake_pos,int snake_size)
+{
+    int Length = snake_size;
+    for (int i = 0;i < Length;i++)
+    {   
+        snake[i].setSize(Vector2f(10.f,10.f));
+        snake[i].setFillColor(sf::Color(150, 50, 250));
+        snake[i].setPosition(Snake_pos[i].position.x,Snake_pos[i].position.y);
+    }
+}
+
+void move_snake(RectangleShape* snake, VertexArray* Snake_pos, const char* direction,int Length)
+{
+    for (int i = 0;i < Length-1;i++)
+    {   
+        Snake_pos[0][i].position = {Snake_pos[0][i+1].position.x, Snake_pos[0][i+1].position.y};
+        snake[i].setPosition(Snake_pos[0][i].position.x,Snake_pos[0][i].position.y);
+    }
+    if (direction == "down")
+    {
+        Snake_pos[0][Length-1].position.y += 10;
+        snake[Length-1].setPosition(Snake_pos[0][Length-1].position.x,Snake_pos[0][Length-1].position.y);
+    }
+    else if (direction == "up")
+    {
+        Snake_pos[0][Length-1].position.y -= 10;
+        snake[Length-1].setPosition(Snake_pos[0][Length-1].position.x,Snake_pos[0][Length-1].position.y);
+    }
+    else if (direction == "left")
+    {
+        Snake_pos[0][Length-1].position.x -= 10;
+        snake[Length-1].setPosition(Snake_pos[0][Length-1].position.x,Snake_pos[0][Length-1].position.y);
+    }
+    else if (direction == "right")
+    {
+        Snake_pos[0][Length-1].position.x += 10;
+        snake[Length-1].setPosition(Snake_pos[0][Length-1].position.x,Snake_pos[0][Length-1].position.y);
+    } 
+} 
+
+void Resize_Snake(RectangleShape* snake, VertexArray* Snake_pos, int new_size,int new_snake_end[2])
+{
+    Snake_pos[0][new_size-1].position = {new_snake_end[0], new_snake_end[1]};
+    snake[new_size-1].setPosition(Snake_pos[0][new_size-1].position.x,Snake_pos[0][new_size-1].position.y);
+}
+
+
 
 int main()
 {
     RenderWindow window(VideoMode(SIZE_X, SIZE_Y), "Snake Game",Style::Close);
 
     bool Loose = false;
-    Vertex Snake_pos;
-    Snake_pos.position = {SIZE_X/2,SIZE_Y/2};
-    RectangleShape snake(Vector2f(10,10));
-    snake.setFillColor(sf::Color(150, 50, 250));
-    snake.setPosition(Snake_pos.position.x,Snake_pos.position.y);
-    window.draw(snake);
+    int current_snake_size = 5;
+    VertexArray Snake_pos(Points,MAX_SNAKE_SIZE);
+    // Initial Position
+    Snake_pos[0].position = {SIZE_X/2,SIZE_Y/2};
+    Snake_pos[1].position = {SIZE_X/2+10,SIZE_Y/2};
+    Snake_pos[2].position = {SIZE_X/2-10,SIZE_Y/2};
+    Snake_pos[3].position = {SIZE_X/2-20,SIZE_Y/2};
+    Snake_pos[4].position = {SIZE_X/2+20,SIZE_Y/2};
+    RectangleShape snake[MAX_SNAKE_SIZE];
+    draw_snake(&snake[0],Snake_pos,current_snake_size);
     int Random_Rec[2] = {0,0};
     Random_Rec[0] = rand()%int(window.getSize().x);
     Random_Rec[1] = rand()%int(window.getSize().y);
@@ -28,46 +83,61 @@ int main()
     RectangleShape Snake_Food(Vector2f(10,10));
     Snake_Food.setFillColor(sf::Color(255, 255, 0));
     Snake_Food.setPosition(Random_Rec[0],Random_Rec[1]);
+    for (int i = 0;i < current_snake_size;i++)
+    {   
+        window.draw(snake[i]);
+    }
+    window.display();
     while (Loose == false)
     {
         Event event;
-        if (Snake_pos.position.x < 0 || Snake_pos.position.x >= window.getSize().x || Snake_pos.position.y < 0 || Snake_pos.position.y >= window.getSize().y)
-           Loose = true;
+        if (Snake_pos[current_snake_size-1].position.x < 0 || Snake_pos[current_snake_size-1].position.x >= window.getSize().x || Snake_pos[current_snake_size-1].position.y < 0 || Snake_pos[current_snake_size-1].position.y >= window.getSize().y)
+            Loose = true; 
         else 
-        { 
+        {  
             while (window.pollEvent(event))
             {
                 if (event.type == Event::Closed)
+                {
                     window.close();
+                } 
                 if (event.key.code == Keyboard::Down)
-                    Snake_pos.position.y += 10; 
+                    move_snake(&snake[0],&Snake_pos,"down",current_snake_size);
                     // Key up pressed
                 if (event.key.code == Keyboard::Up)
-                    Snake_pos.position.y -= 10; 
+                    move_snake(&snake[0],&Snake_pos,"up",current_snake_size);
                     // Key left pressed
                 if (event.key.code == Keyboard::Left)
-                    Snake_pos.position.x -= 10;
+                    move_snake(&snake[0],&Snake_pos,"left",current_snake_size);
                     // Key right pressed
                 if (event.key.code == Keyboard::Right)
-                    Snake_pos.position.x += 10; 
-            }
-            snake.setPosition(Snake_pos.position.x,Snake_pos.position.y);
+                    move_snake(&snake[0],&Snake_pos,"right",current_snake_size);  
+            } 
             // Sample new food position for the snake
-            if (Snake_pos.position.x == Random_Rec[0] && Snake_pos.position.y == Random_Rec[1])
+            if (Snake_pos[current_snake_size-1].position.x == Random_Rec[0] && Snake_pos[current_snake_size-1].position.y == Random_Rec[1])
             {
+                current_snake_size += 1; 
+                Resize_Snake(&snake[0],&Snake_pos,current_snake_size,Random_Rec);
                 Random_Rec[0] = rand()%int(window.getSize().x);
                 Random_Rec[1] = rand()%int(window.getSize().y);
                 Random_Rec[0] -=  Random_Rec[0]%10;
                 Random_Rec[1] -=  Random_Rec[1]%10;
                 Snake_Food.setFillColor(sf::Color(255, 255, 0));
                 Snake_Food.setPosition(Random_Rec[0],Random_Rec[1]);
-            }
+            } 
             window.clear();
-            window.draw(snake);
+            draw_snake(&snake[0],Snake_pos,current_snake_size);
+            for (int i = 0;i < current_snake_size;i++)
+            {   
+                window.draw(snake[i]);
+            } 
             window.draw(Snake_Food);
             window.display();
         }
+        
     }
-    std::cout << Snake_pos.position.x << window.getSize().y   ;
+    std::cout << Snake_pos[4].position.x << window.getSize().y   ;
     return 0;
 }
+
+
